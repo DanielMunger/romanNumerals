0b64605ad0e3bda17ac513d5a5a69d6540b46f03
daniel munger and ezra sandzer-bell <ezra@>
2016-10-27 09:31:02 -0700

    add javascript function to set upper and lower limits for numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 // Business End Logic
-function numConverter(inputOne) {
-  var
+var numberInput;
+var numberOutput
+var result;
+var numbers = [];
+var byNumber = ""
 
+//Function to convert inputed number to a Roman Numeral.
+var numConverter = function(inputOne) {
+  numbers = inputOne.split("");//Creates array of numbers.
+  console.log(numbers);
+  for(i=0; i<numbers.length; i++){
+    if(/[0-9]/.test(numbers) === true && (numbers.length <= 4)) {//tests for valid input.
+      if(numbers[0] >= 4 && numbers.length === 4 || numbers[0] == 0 && numbers.length === 1){//sets an upper and lower limit for input.
+        return("Please Enter a Valid Number.")
+      }
+      else{
+        if()
+      }
+    }
+    else {
+      return("Please Enter a Valid Number.");
+    }
+  }
+};
 // user interface logic
 $(document).ready(function() {
   $("form#romanNumeralForm").submit(function(event) {
     event.preventDefault();
 
-    var numberInput = parseInt($("input#romanNuneral").val());
-    var numberOutput = numConverter(numberInput);
-    var result = numConverter(numberInput);
+    numberInput = $("input#romanNumeral").val();
+    numberOutput = numConverter(numberInput, byNumber);
+    result = numConverter(numberInput);
     $(".converted").text(result);
 
     $("#result").show();
