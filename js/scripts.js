3e4020e35ecd09cab668a5d33fb9c80a762bbce3
daniel munger and ezra sandzer-bell <daniel@>
2016-10-27 14:03:49 -0700

    Final git

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,6 @@ var romanNumerals = [["","I","II","III","IV","V","VI","VII","VIII","IX"],
                     ["","M","MM","MMM"]];
 
 //Function to convert inputed number to a Roman Numeral.
-
 var numConverter = function(inputOne) {
   if(inputOne >= 4000 || inputOne<=0){
     return("Please Enter a Valid Number");
@@ -33,42 +32,6 @@ var numConverter = function(inputOne) {
 
 };
 
-
-
-
-
-
-
-
-
-
-
-//   strOfNumbers = inputOne.split("");//Creates array of numbers.
-//   // console.log(strOfNumbers);
-//   for(i=0; i<strOfNumbers.length; i++){
-//     if(/[0-9]/.test(strOfNumbers) === true && (strOfNumbers.length <= 4)) {//tests for valid input.
-//       if(strOfNumbers[0] >= 4 && strOfNumbers.length === 4 || strOfNumbers[0] == 0 && strOfNumbers.length === 1){//sets an upper and lower limit for input.
-//         return("Please Enter a Valid Number.")
-//       }
-//       else{
-//         for(i=0; i<strOfNumbers.length; i++){//creates array of ints
-//           numbers[i] = parseInt(strOfNumbers[i]);
-//
-//         }
-//         numbers.reverse();
-//         for(i=0; i<numbers.length; i++){
-//           romanOutput = romanNumerals[i][numbers[i]];
-//           //console.log(romanOutput);
-//         }
-//         //romanOutput.reverse();
-//         console.log(romanOutput);
-//       }
-//     }
-//     else {
-//       return("Please Enter a Valid Number.");
-//     }
-//   }
-// };
 // user interface logic
 $(document).ready(function() {
   $("form#romanNumeralForm").submit(function(event) {
