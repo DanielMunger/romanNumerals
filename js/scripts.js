21a28176f99ae874684bf657f60b90bfff81c030
daniel munger and ezra sandzer-bell <daniel@>
2016-10-27 13:25:21 -0700

    add javascript functions. Will restart project with new route.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
 // Business End Logic
 var numberInput;
 var numberOutput
-var result;
+var romanOutput = [];
+var strOfNumbers = [];
 var numbers = [];
 var byNumber = ""
+var romanNumerals = [["","I","II","III","IV","V","VI","VII","VIII","IX"],
+                    ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"],
+                    ["","C","CC","CCC","CD","D","CD","CDD","CDDD","CM"],
+                    ["","M","MM","MMM"]];
 
 //Function to convert inputed number to a Roman Numeral.
 var numConverter = function(inputOne) {
-  numbers = inputOne.split("");//Creates array of numbers.
-  console.log(numbers);
-  for(i=0; i<numbers.length; i++){
-    if(/[0-9]/.test(numbers) === true && (numbers.length <= 4)) {//tests for valid input.
-      if(numbers[0] >= 4 && numbers.length === 4 || numbers[0] == 0 && numbers.length === 1){//sets an upper and lower limit for input.
+  strOfNumbers = inputOne.split("");//Creates array of numbers.
+  // console.log(strOfNumbers);
+  for(i=0; i<strOfNumbers.length; i++){
+    if(/[0-9]/.test(strOfNumbers) === true && (strOfNumbers.length <= 4)) {//tests for valid input.
+      if(strOfNumbers[0] >= 4 && strOfNumbers.length === 4 || strOfNumbers[0] == 0 && strOfNumbers.length === 1){//sets an upper and lower limit for input.
         return("Please Enter a Valid Number.")
       }
       else{
-        if()
+        for(i=0; i<strOfNumbers.length; i++){//creates array of ints
+          numbers[i] = parseInt(strOfNumbers[i]);
+
+        }
+        numbers.reverse();
+        for(i=0; i<numbers.length; i++){
+          romanOutput = romanNumerals[i][numbers[i]];
+          //console.log(romanOutput);
+        }
+        //romanOutput.reverse();
+        console.log(romanOutput);
       }
     }
     else {
@@ -28,7 +43,7 @@ $(document).ready(function() {
   $("form#romanNumeralForm").submit(function(event) {
     event.preventDefault();
 
-    numberInput = $("input#romanNumeral").val();
+    numberInput = parseInt$("input#romanNumeral").val());
     numberOutput = numConverter(numberInput, byNumber);
     result = numConverter(numberInput);
     $(".converted").text(result);
